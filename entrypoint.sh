114ef6409f27d763e3dc73d203ed53729e4cf7b7
madrien-variant <65969027+madrien-variant@users.noreply.github.com>
2020-06-10 17:06:37 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ echo "$INPUT_REMOTE_KEY" | agent-add
 
 # Add strict errors and deploy.
 set -eu
-sh -c "rsync $INPUT_SWITCHES -e 'ssh -o StrictHostKeyChecking=no -p $INPUT_REMOTE_PORT $INPUT_RSH' $GITHUB_WORKSPACE/$INPUT_PATH $INPUT_REMOTE_USER@$INPUT_REMOTE_HOST:$INPUT_REMOTE_PATH"
+sh -c "rsync $INPUT_SWITCHES -e 'ssh -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/$INPUT_PATH $INPUT_REMOTE_USER@$INPUT_REMOTE_HOST:$INPUT_REMOTE_PATH"
